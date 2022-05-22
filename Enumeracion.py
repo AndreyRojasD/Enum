619f100827146460d734af26578abea99697ce25
Andrey-svg <alonsorojasdelga@hotmail.com>
2022-05-22 11:50:25 -0600

    Inicio

---
+++ Enumeracion.py
@@ -0,0 +1,8 @@
+import subprocess
+print("Intruduzca la ip Objetivo")
+target = input()
+print("Objetivo" , target)
+print("Iniciamos filtrando los puertos abiertos")
+subprocess.run(["nmap", "-p-", "--open", "-sS", "--min-rate","5000", "-vvv", "-n", target , "-oG", "PuertoAbiertos"])
+print("Ahora Analizaremos las vulnerabilidades")
+subprocess.run(["nmap", "-v" ,"-sS" "vulnerabilidades.xml" , "--stylesheet=""https://svn.nmap.org/nmap/docs/nmap.xsl""", "--script=vuln", target])
