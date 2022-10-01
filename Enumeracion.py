2d41ebd49489c9287a698c83eddace57f7e9a8db
Andrey <62668026+AndreyRojasD@users.noreply.github.com>
2022-09-30 23:13:56 -0600

    Update Enumeracion.py

--- Enumeracion.py
+++ Enumeracion.py
@@ -1,8 +1,8 @@
 import subprocess
-print("Intruduzca la ip Objetivo")
+print("Enter the target ip")
 target = input()
-print("Objetivo" , target)
-print("Iniciamos filtrando los puertos abiertos")
-subprocess.run(["nmap", "-p-", "--open", "-sS", "--min-rate","5000", "-vvv", "-n", target , "-oG", "PuertoAbiertos"])
+print("Target" , target)
+print("Filtering open ports")
+subprocess.run(["nmap", "-p-", "--open", "-sS", "--min-rate","5000", "-vvv", "-n", target , "-oG", "Open Ports"])
 print("Ahora Analizaremos las vulnerabilidades")
 subprocess.run(["nmap", "-v" ,"-sS","vulnerabilidades.xml" , "--stylesheet=https://svn.nmap.org/nmap/docs/nmap.xsl", "--script=vuln", target])
